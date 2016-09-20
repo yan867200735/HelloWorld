b11bec21c97e28979ac43680805bf3b562e1bad0
Yanner <867200735@qq.com>
2016-09-20 13:30:32 +0800

    作业3，恩，对，好像前边忘了留言了

---
+++ main.js
@@ -0,0 +1,13 @@
+console.log("ddddddddddddddddddddddddddddddddddd");
+var a = "*";
+for (var i = 0; i <= 3; i++) {
+    if (i = 1) {
+        console.log(" ", " ", a, " ", " ");
+    }
+    if (i = 2) {
+        console.log(" ", a, a, a, " ");
+    }
+    if (i = 3) {
+        console.log(a, a, a, a, a);
+    }
+}
--- main.ts
+++ main.ts
@@ -1,1 +1,13 @@
-console.log('hhhhhhhhhhhh')
+console.log("ddddddddddddddddddddddddddddddddddd");
+var a = "*";
+for(var i = 0;i <= 3;i++){
+    if(i = 1){
+        console.log(" "," ",a," "," ");
+    }
+    if(i = 2){
+        console.log(" ",a,a,a," ");
+    }
+    if(i = 3){
+        console.log(a,a,a,a,a);
+    }
+}
